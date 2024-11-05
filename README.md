67a8ffad1edd0674d902f1c0b1af0f2bd3ae3cdc
nailasyarifa <nailasyarifa05@gmail.com>
2024-11-05 22:38:43 +0700

    Tugas 7 Done

--- README.md
+++ README.md
@@ -1,16 +1,43 @@
-# syreattire
+# Tugas 7 :  Elemen Dasar Flutter
+Naila Syarifa Yosarvi - 2306245882
 
-A new Flutter project.
+## 1. Jelaskan apa yang dimaksud dengan stateless widget dan stateful widget, dan jelaskan perbedaan dari keduanya. 
+Stateless widget adalah widget yang tidak memeiliki keadaan state yang dapat berubah. Tampilan dan karakteristik widget ini bersifat tetap dan tidak berubah seiring waktu, serta tidak dipengaruhi oleh perubahan nilai atau interaksi pengguna. Stateless widet bersifat statis dan tidak berubah setelah dibuat. Sedangkan stateful widget adalah widget yang memiliki keadaan state yang dapat berubah selama siklus hidupnya. Stateful widget dapat merender diri mereka sendiri ketika terjadi perubahan dalam statenya. Stateful widget bersifat dinamis dan dapat diperbarui menggunakan. 
 
-## Getting Started
+ ## 2. Sebutkan widget apa saja yang kamu gunakan pada proyek ini dan jelaskan fungsinya.
+main.dart : - MyApp
+                Widget utama yang digunakan sebagai entry point dari aplikasi ini. MyApp adalah turunan dari StatelessWidget.
 
-This project is a starting point for a Flutter application.
+            - MaterialApp
+                MaterialApp adalah widget yang digunakan sebagai root dari seluruh antarmuka aplikasi Flutter. Ini adalah widget yang pertama kali dibangun dalam pohon widget, dan mengatur banyak konfigurasi yang mempengaruhi seluruh aplikasi.
 
-A few resources to get you started if this is your first Flutter project:
+            - MyHomePage
+                Widget halaman utama dari aplikasi ini.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+menu.dart : - Scaffold
+                Menyediakan struktur dasar halaman, termasuk bagian AppBar, body, dan floatingActionButton.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+            - AppBar
+                Bagian atas halaman yang menampilkan judul aplikasi, dalam hal ini "SyreAttire".
+            
+            - Text
+                Text berfungsi untuk menampilkan sebuah teks biasa, atau bisa kita berikan style dengan menambahkan property style.
+            
+            - InfoCard
+                Widget khusus yang menampilkan informasi berupa judul dan isi (seperti NPM, nama, kelas) dalam format kartu.
+            
+            - GridView.count
+                Menampilkan daftar widget dalam bentuk grid dengan jumlah kolom yang ditentukan 
+        
+ ## 3. Apa fungsi dari setState() ? Jelaskan variabel apa saja yang dapat terdampak dengan fungsi tersebut.
+ setState() memungkinkan aplikasi untuk menanggapi perubahan data dan memperbarui tampilan UI secara real-time. Semua variabel yang memengaruhi tampilan UI dalam metode build() dapat diperbarui melalui setState(), sehingga membuat UI menjadi interaktif dan responsif terhadap aksi pengguna.
+
+ ## 4. Jelaskan perbedaan antara const dengan final. 
+ cost digunakan untuk nilai yang benar-benar konstan dan tidak berubah sepanjang waktu kompilasi dan eksekusi, harus diketahui dan diinisialisasi pada waktu kompilasi, sedangkan final digunakan untuk nilai yang tetap setelah diinisialisasi tetapi baru diketahui saat runtime, dan tidak bisa diubah setelah inisialisasi. 
+ 
+ ## 5. Jelaskan bagaimana cara kamu mengimplementasikan checklist-checklist di atas.
+- Buat Proyek Flutter
+- Buat file baru dengan nama menu.dart pada folder game_sthar/lib, kemudian tambahkan import 'package:flutter/material.dart';
+- Memindah dan modifikasi MyHomePage dari main.dart.
+- Mengubah bagian colorSceheme menjadi colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo), pada main.dart
+- Membuat widget stateless baru yang berfungsi untuk menampilkan card.
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,50 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xFF9b957f), // Warna utama
-          secondary: const Color(0xFF9b957f), // Warna sekunder
-        ),
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
 class MyHomePage extends StatelessWidget {
-  final String npm = '2306245882';
-  final String name = 'Naila Syarifa Yosarvi';
-  final String className = 'PBP B';
+  final String npm = '2306245882'; // NPM
+  final String name = 'Naila Syarifa Yosarvi'; // Nama
+  final String className = 'PBP B'; // Kelas
 
   final List<ItemHomepage> items = [
-    ItemHomepage("Lihat Item", Icons.checkroom),
+    ItemHomepage("Lihat Item", Icons.mood),
     ItemHomepage("Tambah Item", Icons.add),
     ItemHomepage("Logout", Icons.logout),
-  ];
-
-  final List<Color> itemColors = [
-    const Color.fromARGB(255, 74, 56, 35),
-    const Color.fromARGB(255, 111, 90, 64),
-    const Color.fromARGB(208, 182, 161, 119),
   ];
 
   MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
     return Scaffold(
+      // AppBar adalah bagian atas halaman yang menampilkan judul.
       appBar: AppBar(
+        // Judul aplikasi "SyreAttire" dengan teks putih dan tebal.
         title: const Text(
           'SyreAttire',
           style: TextStyle(
@@ -52,13 +27,17 @@ class MyHomePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
+      // Body halaman dengan padding di sekelilingnya.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
+        // Menyusun widget secara vertikal dalam sebuah kolom.
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Row untuk menampilkan 3 InfoCard secara horizontal.
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -67,10 +46,17 @@ class MyHomePage extends StatelessWidget {
                 InfoCard(title: 'Class', content: className),
               ],
             ),
+
+            // Memberikan jarak vertikal 16 unit.
             const SizedBox(height: 16.0),
+
+            // Menempatkan widget berikutnya di tengah halaman.
             Center(
               child: Column(
+                // Menyusun teks dan grid item secara vertikal.
+
                 children: [
+                  // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
@@ -81,18 +67,20 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                   ),
+
+                  // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
                   GridView.count(
                     primary: true,
                     padding: const EdgeInsets.all(20),
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
+                    // Agar grid menyesuaikan tinggi kontennya.
                     shrinkWrap: true,
-                    children: items.asMap().entries.map((entry) {
-                      int index = entry.key;
-                      ItemHomepage item = entry.value;
-                      Color color = itemColors[index % itemColors.length];
-                      return ItemCard(item: item, color: color);
+
+                    // Menampilkan ItemCard untuk setiap item dalam list items.
+                    children: items.map((ItemHomepage item) {
+                      return ItemCard(item);
                     }).toList(),
                   ),
                 ],
@@ -106,18 +94,24 @@ class MyHomePage extends StatelessWidget {
 }
 
 class InfoCard extends StatelessWidget {
-  final String title;
-  final String content;
+  // Kartu informasi yang menampilkan title dan content.
+
+  final String title; // Judul kartu.
+  final String content; // Isi kartu.
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
       elevation: 2.0,
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.5,
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width /
+            3.5, // menyesuaikan dengan lebar device yang digunakan.
         padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
         child: Column(
           children: [
             Text(
@@ -141,27 +135,35 @@ class ItemHomepage {
 }
 
 class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
-  final Color color;
+  // Menampilkan kartu dengan ikon dan nama.
 
-  const ItemCard({required this.item, required this.color, super.key});
+  final ItemHomepage item;
+
+  const ItemCard(this.item, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: color,
+      // Menentukan warna latar belakang dari tema aplikasi.
+      color: Theme.of(context).colorScheme.secondary,
+      // Membuat sudut kartu melengkung.
       borderRadius: BorderRadius.circular(12),
+
       child: InkWell(
+        // Aksi ketika kartu ditekan.
         onTap: () {
+          // Menampilkan pesan SnackBar saat kartu ditekan.
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
         },
+        // Container untuk menyimpan Icon dan Text
         child: Container(
           padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
+              // Menyusun ikon dan teks di tengah kartu.
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
